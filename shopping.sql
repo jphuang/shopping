a719de1db0abe93c8bd3aa4eb2509d936a64be2d
jiangping <jphu@qq.com>
2013-04-20 09:51:45 +0800

    提交建库语句
    建立了一个库shopping
    在库中建立几个表

---
+++ shopping.sql
@@ -0,0 +1,53 @@
+--数据库命名全部小写
+create database shopping;
+
+use shopping;
+
+create table tbl_user
+(
+	id int primary key auto_increment,
+	username varchar(40),
+	password varchar(32),
+	phone varchar(40),
+	addr varchar(255),
+	rdate datetime
+);
+
+create table tbl_category
+(
+	id int primary key auto_increment,
+	name varchar(255),
+	descr varchar(255),
+	pid int,
+	isleaf int ,#0表示leaf 1表示非leaf
+	grade int
+);
+
+create table tbl_product
+(
+	id int primary key auto_increment,
+	name varchar(255),
+	descr varchar(255),
+	normalprice double,
+	memberprice double,
+	pdate datetime,
+	categoryid int references category(id)
+);
+
+create table tbl_salesorder
+(
+	id int primary key auto_increment,
+	userid int,
+	addr varchar(255),
+	odate datatime,
+	status int
+);
+
+create table tbl_salesitem
+(
+	id int primary key auto_increment,
+	productid int,
+	unitprice double,
+	pcount int,
+	orderid int
+);

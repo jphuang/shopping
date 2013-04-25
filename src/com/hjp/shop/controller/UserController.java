c61abd4308890b5804355c2971bbb9f4eeda7cc7
jphuang <jphu@qq.com>
2013-04-25 09:06:42 +0800

    Revert "晚了"
    This reverts commit a22c14d78a19d886ca535d617b46e762c1528506.

--- src/com/hjp/shop/controller/UserController.java
+++ src/com/hjp/shop/controller/UserController.java
@@ -4,12 +4,6 @@ import com.jfinal.core.Controller;
 
 public class UserController extends Controller {
 	public void index() {
-		String usernameString =  this.getPara("username");
-		String password = this.getPara("password");
-		String phone = this.getPara("phone");
-		String addr = this.getPara("addr");
-		
-		System.out.println(usernameString + password + phone +  addr);
 		this.renderHtml("<h1>Hello,Welcome To MyShop</h1>");
 
 	}

8b6ff05cd4c3d50255fe04ff04501cf8d79c71bf
jphuang <jphu@qq.com>
2013-05-04 01:15:48 +0800

    重新提交 几个新件

---
+++ src/com/hjp/shop/Interceptor/AdminInterceptor.java
@@ -0,0 +1,18 @@
+package com.hjp.shop.Interceptor;
+
+import com.hjp.shop.model.User;
+import com.jfinal.aop.Interceptor;
+import com.jfinal.core.ActionInvocation;
+
+public class AdminInterceptor implements Interceptor {
+
+	public void intercept(ActionInvocation ai) {
+		User user = (User) ai.getController().getSessionAttr("user");
+		if (user != null && user.get("username").equals("admins")) {
+			ai.invoke();
+		} else {
+			ai.getController().redirect("/admin/login");
+		}
+	}
+
+}

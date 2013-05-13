8e22d2a1132641af66438d4c6638561144c7deda
jphuang <jphu@qq.com>
2013-05-14 01:22:15 +0800

    main config update

--- src/config/ShopConfig.java
+++ src/config/ShopConfig.java
@@ -2,13 +2,14 @@
 package config;
 
 import com.hjp.shop.controller.AdminControlller;
+import com.hjp.shop.controller.CategoryController;
 import com.hjp.shop.controller.IndexController;
 import com.hjp.shop.controller.UserController;
+import com.hjp.shop.model.Category;
 import com.hjp.shop.model.User;
 import com.jfinal.config.*;
 import com.jfinal.plugin.activerecord.ActiveRecordPlugin;
 import com.jfinal.plugin.c3p0.C3p0Plugin;
-import com.jfinal.render.ViewType;
 public class ShopConfig extends JFinalConfig{
 
 	@Override
@@ -21,6 +22,7 @@ public class ShopConfig extends JFinalConfig{
 		me.add("/user",UserController.class);
 		me.add("/admin",AdminControlller.class);
 		me.add("/",IndexController.class);
+		me.add("/category",CategoryController.class);
 		
 	}
 
@@ -31,6 +33,7 @@ public class ShopConfig extends JFinalConfig{
 	    ActiveRecordPlugin arp = new ActiveRecordPlugin(cp); 
 	    me.add(arp); 
 	    arp.addMapping("tbl_user", User.class); 
+	    arp.addMapping("tbl_category", Category.class); 
 		    
 	}
 
